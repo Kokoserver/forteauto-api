940bcda7f8157ec4380444370189a81164104685
Kokoserver <owonikokoolaluwa@gmail.com>
2022-02-25 05:05:15 +0100

    adding sqlite for testing

--- forteauto/database/database_dependencies.py
+++ forteauto/database/database_dependencies.py
@@ -4,9 +4,12 @@ import sqlalchemy
 from forteauto.conf import config as base_config
 
 def get_db_url():
+    if base_config.settings.environment == "test" or "testing":
+        test_db_url =  f"sqlite:///testing.sqlite"
+        return test_db_url
+    else:
         database_url = f"postgresql://{base_config.settings.database_username}:{base_config.settings.database_password}@{base_config.settings.database_host}:{base_config.settings.database_port}/{base_config.settings.database_name}"
         return database_url
-
 
 database_url = get_db_url()
 database = databases.Database(database_url)
