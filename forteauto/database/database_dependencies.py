8097cf445bf0a13f5ebeb0380131d126fcc0a0b6
Kokoserver <owonikokoolaluwa@gmail.com>
2022-02-25 04:56:45 +0100

    chnaging database url format

--- forteauto/database/database_dependencies.py
+++ forteauto/database/database_dependencies.py
@@ -4,15 +4,10 @@ import sqlalchemy
 from forteauto.conf import config as base_config
 
 def get_db_url():
-    if base_config.settings.environment == "test" or "testing":
-        test_db_url =  f"postgresql://{base_config.settings.database_username}:{base_config.settings.database_password}@{base_config.settings.database_host}:{base_config.settings.database_port}/{base_config.settings.database_name}_test"
-        return test_db_url
-    else:
         database_url = f"postgresql://{base_config.settings.database_username}:{base_config.settings.database_password}@{base_config.settings.database_host}:{base_config.settings.database_port}/{base_config.settings.database_name}"
         return database_url
 
 
-    
 database_url = get_db_url()
 database = databases.Database(database_url)
 metadata = sqlalchemy.MetaData(database)
